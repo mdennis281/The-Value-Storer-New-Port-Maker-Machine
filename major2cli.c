4daa1edf61007a097afd8767270c84bced892c5d
Mason <masonhobbs@my.unt.edu>
2017-11-30 13:12:12 -0600

    client updates,backup

--- major2cli.c
+++ major2cli.c
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<sys/types.h>
 #include<sys/socket.h>
+#include<sys/un.h>
 #include<netinet/in.h>
 #include<unistd.h>
 #include<netdb.h>
@@ -10,6 +11,9 @@
 #include<arpa/inet.h>
 
 typedef enum {false, true} bool;
+
+//define unix socket
+#define SOCKETNAME "local"
 
 int main(int argc, char ** argv)
 {
@@ -20,11 +24,16 @@ int main(int argc, char ** argv)
 	int i, j;		//iterators
 	int total;		//clients total
 
+	bool inetf,		//dealing with inet
+	     unixf;		//dealing with unix
+
 	char c_total[16];	//total as a character
 	char buffer[256];	//buffer for read & write
 
 	struct sockaddr_in r_addr,	//remote address
-			   s_addr;	//server address
+			   i_addr;	//inet server address
+	struct sockaddr_un u_addr;	//unix server address
+
 
 	struct hostent *serv;		//host server
 
@@ -34,23 +43,24 @@ int main(int argc, char ** argv)
 		exit(1);
 	}
 
+	//set values
+	unixf = false;
+	inetf = false;
+
 	//set port number
 	p_no = atoi(argv[2]);
 
 	//create local socket
-//	if(
-//	{
-		s_fd = socket(AF_INET, SOCK_STREAM, 0);
-		if(s_fd < 0)
-		{
-			perror("socket local");
-			exit(1);
-		}
-//	}
-
+/*	s_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(s_fd < 0)
+	{
+		perror("socket local");
+		exit(1);
+	}
+*/
 	//create remote socket
 	r_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if(s_fd < 0)
+	if(r_fd < 0)
 	{
 		perror("socket remote");
 		exit(1);
@@ -58,7 +68,8 @@ int main(int argc, char ** argv)
 
         //zero out server addresses
         bzero((char *) &r_addr, sizeof(r_addr));
-	bzero((char *) &s_addr, sizeof(s_addr));
+	bzero((char *) &i_addr, sizeof(i_addr));
+	bzero((char *) &u_addr, sizeof(u_addr));
 
         //get host server
         serv = gethostbyname(argv[1]);
@@ -68,11 +79,51 @@ int main(int argc, char ** argv)
                 exit(1);
         }
 
+	//AF_UNIX client
+	if((char *)serv->h_addr != argv[3])
+	{
+		unixf = true;
+
+		s_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+		if(s_fd < 0)
+		{
+			perror("socket unix");
+			exit(1);
+		}
+
+		//set server info
+		u_addr.sun_family = AF_UNIX;
+		strcpy(u_addr.sun_path, SOCKETNAME);
+	}
+	//AF_INET client
+	else if((char *)serv->h_addr == argv[3])
+	{
+		inetf = true;
+
+		s_fd = socket(AF_INET, SOCK_STREAM, 0);
+		if(s_fd < 0)
+		{
+			perror("socket inet");
+			exit(1);
+		}
+
+		//set server info now
+		i_addr.sin_family = AF_INET;
+		bcopy(serv->h_addr, (char *)&i_addr.sin_addr.s_addr, serv->h_length);
+		i_addr.sin_port = htons(p_no);
+	}
+	//IP ADDRESS FAIL
+	else
+	{
+		perror("invalid ip");
+		exit(1);
+	}
+
 	//set all local server info
-	s_addr.sin_family = AF_INET;
+/*	s_addr.sin_family = AF_INET;
 	bcopy((char *)serv->h_addr, (char *)&s_addr.sin_addr.s_addr, serv->h_length);
 	s_addr.sin_port = htons(p_no);
-
+*/
 	//bind local
 	/*if(bind(s_fd, (struct sockaddr *)&l_addr, sizeof(l_addr)) < 1)
 	{
@@ -81,7 +132,7 @@ int main(int argc, char ** argv)
 	}*/
 
 	//connect local
-	if(connect(s_fd, (struct sockaddr *)&s_addr, sizeof(s_addr)) < 0)
+	if(connect(s_fd, (struct sockaddr *)&i_addr, sizeof(i_addr)) < 0)
 	{
 		perror("connect server");
 		exit(1);

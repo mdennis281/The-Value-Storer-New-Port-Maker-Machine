10d728ea7df725cbc96c49c2a17a790093d24b95
Mason <masonhobbs@my.unt.edu>
2017-11-30 13:54:34 -0600

    functions header for cleanliness

---
+++ functions.h
@@ -0,0 +1,31 @@
+//*****************************//
+// GENERAL FUNCTIONS AND MISC  //
+//*****************************//
+
+#include<arpa/inet.h>
+#include<netdb.h>
+#include<netinet/in.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/un.h>
+#include<sys/time.h>
+#include<unistd.h>
+
+#define SOCKETNAME "local"
+
+typedef enum {false, true} bool;
+
+char * return_IP(struct hostent * s)
+{
+	char * ip;
+
+	struct in_addr i;
+
+	memcpy(&i, s->h_addr_list[0], sizeof(struct in_addr));
+	strcpy(ip, inet_ntoa(i));
+
+	return ip;
+}
